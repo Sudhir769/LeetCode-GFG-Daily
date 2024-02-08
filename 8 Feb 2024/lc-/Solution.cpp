84dcc1cb48a21e1651a417a9cc6bfd2cf5f7de50
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-02-09 05:15:29 +0530

    8 Feb 2024

--- 8 Feb 2024/lc-/Solution.cpp
+++ 8 Feb 2024/lc-/Solution.cpp
@@ -4,20 +4,44 @@ using namespace std;
 class Solution
 {
 public:
+    vector<int> dp;
+    int solve(int n)
+    {
+        if (n == 0)
+            return 0;
+        if (dp[n] != -1)
+            return dp[n];
+
+        int count = 1e7;
+        for (int i = 1; i * i <= n; i++)
+        {
+            int result = 1 + solve(n - i * i);
+            count = min(count, result);
+        }
+        return dp[n] = count;
+    }
+
     int numSquares(int n)
     {
-        // padding ahead
-        static vector<int> dp = {0};
+        // dp.resize(n+1, -1);
+        // return solve(n);
 
-        while (dp.size() <= n)
+        dp.resize(n + 1, 0);
+        for (int i = 1; i * i <= n; i++)
         {
-            int i = dp.size();
-            int count = INT_MAX;
-            for (int j = 1; j * j <= i; ++j)
+            dp[i * i] = 1;
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (dp[i] == 1)
+                continue;
+            int root = sqrt(i), mn = INT_MAX;
+            for (int j = 1; j <= root; j++)
             {
-                count = min(count, dp[i - j * j] + 1);
+                mn = min(mn, dp[j * j] + dp[i - j * j]);
             }
-            dp.push_back(count);
+            dp[i] = mn;
         }
         return dp[n];
     }
