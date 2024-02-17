cd317940de576701aab415607776def46e2f9cd7
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-02-17 11:01:32 +0530

    17 Feb 2024

---
+++ 17 Feb 2024/gfg/Solution.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution{
+    public:
+    bool isMaxHeap(int arr[], int n)
+    {
+        queue<int>q;
+        q.push(arr[0]);
+        
+        for(int i=1; i<n; i++){
+            int big = q.front();
+            q.pop();
+            
+            if(arr[i] > big){
+                return 0;
+            }
+            q.push(arr[i]);
+            
+            if(i+1 < n){
+                if(arr[i+1] > big){
+                    return 0;
+                }
+                q.push(arr[i+1]);
+                i++;
+            }
+        }
+        return true;
+    }
+};
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        
+    }
+}
+
