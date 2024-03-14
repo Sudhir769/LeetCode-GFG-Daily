951c2ecfe8243ce0701e73254116437126128a4e
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-03-15 01:48:34 +0530

    14 March 2024

---
+++ 14 March 2024/lc-/Solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int> &nums, int goal)
+    {
+        unordered_map<int, int> mp;
+
+        int result = 0, curr = 0;
+        mp[0] = 1;
+        for (auto num : nums)
+        {
+            curr += num;
+            int rem = curr - goal;
+
+            if (mp.find(rem) != mp.end())
+            {
+                result += mp[rem];
+            }
+
+            mp[curr]++;
+        }
+        return result;
+    }
+};
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        
+    }
+}
