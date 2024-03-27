dea6f095ded2f6f75f8b2314d77082d10647e4ec
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-03-27 22:22:32 +0530

    27 March 2024

---
+++ 27 March 2024/lc-/Solution.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int numSubarrayProductLessThanK(vector<int> &v, int k)
+    {
+        int n = v.size();
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int s = 1;
+            for (int j = i; j < n; j++)
+            {
+                s = s * v[j];
+                if (s >= k)
+                    break;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+    }
+}
