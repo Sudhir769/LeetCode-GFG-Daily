f4e73c47b9e9ebc062848cdabd84021c43072b57
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-04-05 23:45:43 +0530

    5 April 2024

--- 5 April 2024/lc-/Solution.cpp
+++ 5 April 2024/lc-/Solution.cpp
@@ -1,15 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    
+    string makeGood(string s)
+    {
+        stack<char> st;
+        ;
+        int n = s.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            if (st.empty())
+                st.push(s[i]);
+            else if (abs(st.top() - s[i]) == 32)
+                st.pop();
+            else
+                st.push(s[i]);
+        }
+
+        string ans = "";
+        while (!st.empty())
+        {
+            ans = st.top() + ans;
+            st.pop();
+        }
+        return ans;
+    }
 };
 
-int main(){
-    int t; cin>>t;
-    while(t--){
-        
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
     }
 }
