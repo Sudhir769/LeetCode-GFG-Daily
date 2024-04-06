134f5ac5743bef2dffde75af13a237d69faa96bf
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-04-06 17:27:41 +0530

    6 April 2024

---
+++ 6 April 2024/gfg/Solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    long long countWays(int n)
+    {
+        return (long long)(n / 2) + 1;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+    }
+}
