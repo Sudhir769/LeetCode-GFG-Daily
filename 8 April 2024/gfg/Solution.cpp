ba86ae64bf313b793bb125d656f349f0040456c2
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-04-08 19:02:50 +0530

    8 April 2024

---
+++ 8 April 2024/gfg/Solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+public:
+    
+};
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        
+    }
+}
+
---
+++ 8 April 2024/lc-/Solution.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int countStudents(vector<int> &students, vector<int> &sandwiches)
+    {
+        int n = students.size();
+        vector<int> cnt(2, 0);
+        for (int i = 0; i < n; i++)
+        {
+            cnt[students[i]]++;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            int sand = sandwiches[i];
+            if (cnt[sand] == 0)
+            {
+                return n - i;
+            }
+            else
+            {
+                cnt[sand]--;
+            }
+        }
+        return 0;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+    }
+}
