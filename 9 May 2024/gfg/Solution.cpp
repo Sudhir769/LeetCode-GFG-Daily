5b1cb8d269d8b8e99a80c0808199053ab00988cf
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-05-09 07:49:09 +0530

    09 May 2024

---
+++ 9 May 2024/gfg/Solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool divisorGame(int n)
+    {
+        return (n % 2 == 0);
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+    }
+}
---
+++ 9 May 2024/lc-/Solution.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    long long maximumHappinessSum(vector<int> &happiness, int k)
+    {
+        int n = happiness.size();
+
+        sort(happiness.begin(), happiness.end());
+        int i = n;
+        long long result = 0;
+        int dec = 0;
+        while (i-- and k > 0)
+        {
+            if (happiness[i] - dec > 0)
+            {
+                result += (happiness[i] - dec); //[1,2,3]
+                dec++;
+                k--;
+            }
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+    }
+}
