9dacea33c3f56b90316d63cb504856ddd69a74ca
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-06-09 09:24:52 +0530

    09 June 2024

---
+++ 09 June 2024/gfg/Solution.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+public:
+    void zigZag(int n, vector<int> &arr)
+    {
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (i % 2 == 0)
+            {
+                if (arr[i] > arr[i + 1])
+                    swap(arr[i], arr[i + 1]);
+            }
+            else
+            {
+                if (arr[i] < arr[i + 1])
+                    swap(arr[i], arr[i + 1]);
+            }
+        }
+    }
+};
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        
+    }
+}
+
