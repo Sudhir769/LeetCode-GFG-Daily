f00747c31aaf4912bc48deed8a024771412c8047
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-06-10 18:13:23 +0530

    10 June 2024

---
+++ 10 June 2024/gfg/Solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+public:
+    void matchPairs(int n, char nuts[], char bolts[])
+    {
+        sort(nuts, nuts + n);
+        sort(bolts, bolts + n);
+    }
+};
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        
+    }
+}
+
