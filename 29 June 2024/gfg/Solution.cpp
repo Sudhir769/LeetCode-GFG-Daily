dbadc9d64825497e8914914917dd51c594216f74
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-06-29 12:47:44 +0530

    29 June 2024

---
+++ 29 June 2024/gfg/Solution.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Structure of the node of the linked list is as
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+class Solution {
+public:
+    bool areIdentical(struct Node *head1, struct Node *head2)
+    {
+        while (head1 && head2)
+        {
+            if (head1->data != head2->data)
+                return false;
+
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+        if (head1 || head2)
+            return false;
+
+        return true;
+    }
+};
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        
+    }
+}
+
