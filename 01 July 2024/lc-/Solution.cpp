2572f7850693913e568739959cf260b92181f7a4
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-07-01 10:29:17 +0530

    01 July 2024

--- 01 July 2024/lc-/Solution.cpp
+++ 01 July 2024/lc-/Solution.cpp
@@ -4,7 +4,25 @@ using namespace std;
 
 class Solution {
 public:
-    
+    bool threeConsecutiveOdds(vector<int> &arr)
+    {
+        int n = arr.size();
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] % 2)
+            {
+                cnt++;
+                if (cnt == 3)
+                    return true;
+            }
+            else
+            {
+                cnt = 0;
+            }
+        }
+        return false;
+    }
 };
 
 int main(){
