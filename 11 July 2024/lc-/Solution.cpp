79bc936d2c8c4405740cccef1b94d3f27750c53c
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-07-11 22:07:04 +0530

    11 July 2024

---
+++ 11 July 2024/lc-/Solution.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string reverseParentheses(string s)
+    {
+        int n = s.length();
+        stack<int> st;
+        vector<int> index(n, -1);
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '(')
+            {
+                st.push(i);
+            }
+            else if (s[i] == ')')
+            {
+                int j = st.top();
+                st.pop();
+
+                index[i] = j;
+                index[j] = i;
+            }
+        }
+
+        string result = "";
+        int dir = 1;
+
+        for (int i = 0; i < n; i += dir)
+        {
+            if (s[i] == '(' or s[i] == ')')
+            {
+                i = index[i];
+                dir = -dir;
+            }
+            else
+            {
+                result.push_back(s[i]);
+            }
+        }
+        return result;
+    }
+};
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        
+    }
+}
