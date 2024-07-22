6c26aadeafe848b1bfddd7ed8ad6a4e2f9529106
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-07-22 10:29:52 +0530

    22 July 2024

--- 22 July 2024/lc-/Solution.cpp
+++ 22 July 2024/lc-/Solution.cpp
@@ -4,21 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-    unordered_map<int, string> mp;
-    int n = names.size();
-
-    for (int i = 0; i < n; i++)
+    vector<string> sortPeople(vector<string> &names, vector<int> &heights)
     {
-        mp[heights[i]] = names[i];
-    }
-    sort(heights.rbegin(), heights.rend());
-    vector<string> ans;
+        unordered_map<int, string> mp;
+        int n = names.size();
 
-    for (int i = 0; i < n; i++)
-    {
-        ans.push_back(mp[heights[i]]);
+        for (int i = 0; i < n; i++)
+        {
+            mp[heights[i]] = names[i];
+        }
+        sort(heights.rbegin(), heights.rend());
+        vector<string> ans;
+
+        for (int i = 0; i < n; i++)
+        {
+            ans.push_back(mp[heights[i]]);
+        }
+        return ans;
     }
-    return ans;
 };
 
 int main(){
