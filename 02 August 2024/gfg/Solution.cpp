60623e2f076536456848a60143340b850235cc01
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-08-02 14:27:42 +0530

    02 August 2024

---
+++ 02 August 2024/gfg/Solution.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+public:
+    int dp[101][101];
+    int solve(int i, int j, string &s1, string &s2)
+    {
+
+        if (i < 0)
+            return j + 1;
+        if (j < 0)
+            return i + 1;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (s1[i] == s2[j])
+        {
+            return dp[i][j] = 0 + solve(i - 1, j - 1, s1, s2);
+        }
+
+        return dp[i][j] = 1 + min({solve(i - 1, j, s1, s2), solve(i, j - 1, s1, s2), solve(i - 1, j - 1, s1, s2)});
+    }
+
+    int editDistance(string str1, string str2)
+    {
+
+        memset(dp, -1, sizeof(dp));
+        return solve(str1.length() - 1, str2.length() - 1, str1, str2);
+    }
+};
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        
+    }
+}
+
