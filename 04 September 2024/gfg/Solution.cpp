0d6a7600c5c8c58c1b6e6f1360a7d93ebdad0a6f
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-09-04 19:52:58 +0530

    04 September 2024

---
+++ 04 September 2024/gfg/Solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int nthStair(int n)
+    {
+        return n / 2 + 1;
+    }
+};
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        
+    }
+}
+
