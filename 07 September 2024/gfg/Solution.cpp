ce9096456b6366f7b517bfdbb7d7b3ecaef6217e
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-09-07 11:06:46 +0530

    07 September 2024

---
+++ 07 September 2024/gfg/Solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    long long findNth(long long n)
+    {
+        long long ans = 0;
+        long long p = 1;
+
+        while (n > 0)
+        {
+            ans += p * (n % 9);
+            n /= 9;
+            p *= 10;
+        }
+        return ans;
+    }
+};
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        
+    }
+}
+
