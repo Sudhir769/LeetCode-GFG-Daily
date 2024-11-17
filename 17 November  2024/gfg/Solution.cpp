8820235e3bb9230ca8658cb02765467a0c39c872
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-11-17 18:45:35 +0530

    17 November 2024

---
+++ 17 November  2024/gfg/Solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void reverseArray(vector<int> &arr)
+    {
+        reverse(arr.begin(), arr.end());
+    }
+};
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        
+    }
+}
+
