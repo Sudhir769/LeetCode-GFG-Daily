ec399018808bbf26cee849a97e1461c842e7f8ef
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-12-02 21:09:18 +0530

    02 December 2024

---
+++ 02 December  2024/gfg/Solution.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> ans;
+    int n, m;
+    void solve(int i, string &text, string &pat)
+    {
+
+        if (i >= m)
+            return;
+
+        auto it = text.find(pat, i);
+        if (it != string::npos)
+        {
+            ans.push_back(it);
+            solve(it + 1, text, pat);
+        }
+    }
+    vector<int> search(string &pat, string &txt)
+    {
+        n = pat.length();
+        m = txt.length();
+        solve(0, txt, pat);
+        return ans;
+    }
+};
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        
+    }
+}
+
---
+++ 02 December  2024/lc-/Solution.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int isPrefixOfWord(string sentence, string searchWord)
+    {
+        stringstream ss(sentence);
+        string token = "";
+
+        int i = 0;
+        while (ss >> token)
+        {
+            i++;
+            if (token.find(searchWord) != string::npos)
+            {
+                int j = 0;
+                bool flag = true;
+                while (j < searchWord.length() and j < token.length())
+                {
+                    if (token[j] != searchWord[j])
+                    {
+                        flag = false;
+                    }
+                    j++;
+                }
+                if (flag)
+                {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        
+    }
+}
