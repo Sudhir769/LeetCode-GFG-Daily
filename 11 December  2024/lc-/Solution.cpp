a62f26520d993a69593708f8dc8af7b06d2a5667
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-12-11 22:38:40 +0530

    11 December 2024

---
+++ 11 December  2024/lc-/Solution.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maximumBeauty(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+
+        vector<vector<int>> intervals;
+        for (int i = 0; i < n; i++)
+        {
+            intervals.push_back({nums[i] - k, nums[i] + k});
+        }
+        sort(intervals.begin(), intervals.end());
+        // {{-1, 3}, {0, 4}, {2, 6}, {4, 8}}
+
+        queue<vector<int>> q;
+
+        int result = 0;
+        for (auto interval : intervals)
+        {
+            while (!q.empty() and q.front()[1] < interval[0])
+            {
+                q.pop();
+            }
+            q.push(interval);
+            result = max(result, (int)q.size());
+        }
+        return result;
+    }
+};
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        
+    }
+}
