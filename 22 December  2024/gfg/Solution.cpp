5b10bb6bba6cc14b5737cb6852678bae5dc9de67
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2024-12-22 16:57:17 +0530

    22 December 2024

---
+++ 22 December  2024/gfg/Solution.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool matSearch(vector<vector<int>> &mat, int x)
+    {
+        int n = mat.size();
+        int m = mat[0].size();
+
+        int i = 0, j = m - 1;
+        while (i < n and j >= 0)
+        {
+            if (mat[i][j] == x)
+            {
+                return true;
+            }
+            else if (mat[i][j] > x)
+            {
+                j--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return false;
+    }
+};
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        
+    }
+}
+
